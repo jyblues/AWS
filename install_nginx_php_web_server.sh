ce4c5d3a258026da898aaf372a7dfbd9e14b58e1
jyblues <jyblues@gmail.com>
2015-12-23 20:24:06 +0900

    php fpm nginx 구성합니다

--- install_nginx_php_web_server.sh
+++ install_nginx_php_web_server.sh
@@ -16,4 +16,38 @@ sudo firewall-cmd --reload
 # PHP
 sudo yum install -y php70w php70w-opcache php70w-common php70w-fpm php70w-mysql php70w-mcrypt php70w-mbstring
 
-# PHP-FPM 연동
+
+# Nginx Configuration
+sudo vi /etc/nginx/conf.d/default.conf
+
+#server{
+#    
+#    location / {
+#        root   /var/www/html;
+#        index  index.php index.html index.htm;
+#    }
+#    
+#    location ~ \.php$ {
+#        fastcgi_pass    unix:/var/run/php-fpm/php-fpm.sock;
+#        fastcgi_index   index.php;
+#        fastcgi_param   SCRIPT_FILENAME  /var/www/html$fastcgi_script_name;
+#        include         fastcgi_params;
+#    }
+#}
+
+
+# php-FPM Configuration
+sudo vi /etc/php-fpm.d/www.conf
+
+# user = nginx
+# group = nginx
+#
+#   ;listen = 127.0.0.1:9000
+# listen = /var/run/php-fpm/php-fpm.sock
+#   ;listen.owner = nobody
+# listen.owner = nginx
+#   ;listen.group = nobody
+# listen.group = nginx
+#   ;listen.mode = 0666
+# listen.mode = 0664
+
