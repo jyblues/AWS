dbe96a80e210ce054685c1327a47191bb14d423e
jyblues <jyblues@gmail.com>
2015-12-23 18:43:51 +0900

    sudo로 작동하게 변경했습니다

--- install_nginx_php_web_server.sh
+++ install_nginx_php_web_server.sh
@@ -3,17 +3,17 @@ sudo yum -y update
 sudo yum install -y gcc make
 
 # CentOS/RHEL 7.x
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
 # nginx 1.8 install
-yum install -y nginx18
+sudo yum install -y nginx18
 
 # HTTP firewall setting
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --reload
 
 # PHP
-yum install -y php70w php70w-opcache php70w-common php70w-fpm php70w-mysql php70w-mcrypt php70w-mbstring
+sudo yum install -y php70w php70w-opcache php70w-common php70w-fpm php70w-mysql php70w-mcrypt php70w-mbstring
 
 # PHP-FPM 연동
