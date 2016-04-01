c91dc6c83da10cb7889a09b91f70ad92bc95d26a
jyblues <jyblues@gmail.com>
2016-04-01 15:29:50 +0900

    Update apache_install.sh

--- apache_install.sh
+++ apache_install.sh
@@ -1,2 +1,6 @@
+# web server 구축
+http://www.spidersoft.com.au/2013/apache-php-config-boilerplate/
+
 # phpize 설치
-sudo yum install -y php-devel
+sudo yum install -y php-devel  # php
+sudo yum install -y php56-devel  # php 5.6
