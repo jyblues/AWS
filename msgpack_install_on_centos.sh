2c2e1ceb073ae66dec763fac5e8b775bc8f919cb
jyblues <jyblues@gmail.com>
2016-05-03 18:15:05 +0900

    Update msgpack_install_on_centos.sh

--- msgpack_install_on_centos.sh
+++ msgpack_install_on_centos.sh
@@ -1,1 +1,5 @@
+# Installation, on Fedora or Enterprise Linux with php 5.3
 yum --enablerepo=remi install php-pecl-msgpack
+
+# on Enterprise Linux with php 5.4
+yum --enablerepo=remi,remi-test install php-pecl-msgpack
